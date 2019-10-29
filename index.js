ca2b67d404cee1e0ce14e2c96d9637633c78294c
bruno <bruno.jeanluke@gmail.com>
2019-10-29 13:36:50 -0400

    polished up the getWindowList() for linux, it now returns a list of Window Objects

--- index.js
+++ index.js
@@ -84,11 +84,35 @@ const getProcessList = async (callback) => {
   callback(processList)
 }
 
-const getWindowList = () => {
+/**
+ * Gets the list of open windows and returns it as an array of window objects.
+ * Each object has a id, user and title attributes
+ * - id is the window id
+ * - user is the user that owns the process running the window
+ * - title is the title of the window
+ * @param {function} callback 
+ */
+const getWindowList = (callback) => {
   if (process.platform === 'linux') {
     const linuxGetWindowList = path.join(__dirname, 'linux', 'getWindowList.sh')
     exec(linuxGetWindowList, (error, stdout, stderr) => {
-      
+      if (error) {
+        callback(error, null)
+        return
+      }
+      if (stderr) {
+        callback(stderr, null)
+        return
+      }
+      windowStrings = stdout.split('\n')
+      windowList = []
+
+      windowStrings.forEach((windowString) => {
+        let windowTitle = windowString.split(' ').slice(4).join(' ')
+        windowObject = {id: windowString.split(' ')[0], user: windowString.split(' ')[3], title: windowTitle}
+        windowList.push(windowObject)
+      })
+      callback(null, windowList)
     })
   } else {
     console.log('platform not supported yet')
--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,5 @@
-// const index = require('./../index')
-const path = require('path')
-const { exec } = require('child_process')
+const index = require('./../index')
+
 
 // // make sure to enter a valid pid
 // index.sendKeys(23561, 'echo "hello"', {resetFocus: true, pressEnterOnceDone: false, callback: (e, m) => {
@@ -11,26 +10,7 @@ const { exec } = require('child_process')
 //   console.log(pslist)
 // })
 
-const getWindowList = (callback) => {
-  if (process.platform === 'linux') {
-    const linuxGetWindowList = path.join(__dirname, 'linux', 'getWindowList.sh')
-    exec(linuxGetWindowList, (error, stdout, stderr) => {
-      if (error) {
-        callback(error, null)
-        return
-      }
-      if (stderr) {
-        callback(stderr, null)
-        return
-      }
-      callback(null, stdout)
-    })
-  } else {
-    console.log('platform not supported yet')
-  }
-}
-
-getWindowList((error, windowList) => {
+index.getWindowList((error, windowList) => {
   console.log(error)
   console.log(windowList)
 })
